17a642ab8e0b495fbf8dd4828e62af344cac3dae
NinjaGamer107 <43365376+NinjaGamer107@users.noreply.github.com>
2020-04-04 11:03:09 +0200

    Finished integrating Vue in the Navbar

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,13 @@
 const app = new Vue({
     el: '#app',
     data: {
-        
+        NavBarElements: [ 'About', 'Create a bill', 'View your bills'],
+        SelectedNavBar: 'About'
+    },
+    methods: {
+        updateNav(index) {
+            this.SelectedNavBar = this.NavBarElements[index]
+            console.log(this.SelectedNavBar)
+        }
     }
 })
