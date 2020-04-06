532db8f570ae68783c1c3e7425a57fe6aef352ee
NinjaGamer107 <43365376+NinjaGamer107@users.noreply.github.com>
2020-04-06 16:05:59 +0200

    Setting everything up for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,21 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project 
+    content: [
+      './index.html',
+      './scripts/index.js'
+    ],
+  
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  })
+
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        ...process.env.NODE_ENV === 'production'
+        ? [purgecss]
+        : []
     ]
 }
